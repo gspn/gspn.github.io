64dc7db14c75435896159c5d890b54b47d7f682e
gspn <ghostspoon@yahoo.com>
2023-09-28 22:09:29 -0400

    start of routing and pages

---
+++ pages/0.js
@@ -0,0 +1,14 @@
+export default {
+	welcome: {
+		title: "GSPN - Welcome",
+		name: "",
+		tags: ["invis"]
+	}
+};
+/**
+ * Spooky lil INSTRUCTIONS! BWAH!
+ * Things to know about the list of articles.
+ * 
+ * the "invis" tag makes the "post" invisible in the list, as it is meant to be used as a static page
+ * (The config page is one of these. So is the welcome page.)
+ */
---
+++ src/page.js
@@ -0,0 +1,20 @@
+// router
+
+import routie from "./routie.min.js"; // ancient, but it works, and it definitely won't change.
+import pages from "/pages/0.js";
+
+const loadPage = function(){
+
+};
+
+const listPages = function({filter, showtags, showinvis}){
+
+}
+
+routie({
+	"": function(){
+
+	}
+})
+
+window.gspnp = {}
